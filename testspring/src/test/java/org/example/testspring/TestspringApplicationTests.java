22a912d85dce197625ca3337c64d46f3d52804cb
son023 <141652370+son023@users.noreply.github.com>
2024-07-23 09:40:32 +0700

    first

---
+++ testspring/src/test/java/org/example/testspring/TestspringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.testspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestspringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
